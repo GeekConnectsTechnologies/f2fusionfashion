c67d74f5f7a908b1b534b7d78caab665928d52f6
Shyam Chhatbar <shyamchhatbar000@gmail.com>
2022-04-16 22:37:51 +0530

    Landing page check done

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/css/style.css">
+    <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
 
     <title>Hello, world!</title>
 </head>
@@ -83,34 +84,34 @@
                 </div>
                 <div class="col-sm-12 col-md-6 col-lg-6 hideScroll text-center" style="height: 950px;  overflow: auto;">
                     <div class="row">
-                        <div class="col-6 col-sm-12 col-md-6 col-lg-6">
+                        <div class="col-6 col-sm-6 col-md-6 col-lg-6">
                             <img class="img-fluid productImage" src="assets/images/square300x300.png">
                         </div>
-                        <div class="col-6 col-sm-12 col-md-6 col-lg-6">
+                        <div class="col-6 col-sm-6 col-md-6 col-lg-6">
                             <img class="img-fluid productImage" src="assets/images/square300x300.png">
                         </div>
-                        <div class="col-6 col-sm-12 col-md-6 col-lg-6">
+                        <div class="col-6 col-sm-6 col-md-6 col-lg-6">
                             <img class="img-fluid productImage" src="assets/images/square300x300.png">
                         </div>
-                        <div class="col-6 col-sm-12 col-md-6 col-lg-6">
+                        <div class="col-6 col-sm-6 col-md-6 col-lg-6">
                             <img class="img-fluid productImage" src="assets/images/square300x300.png">
                         </div>
-                        <div class="col-6 col-sm-12 col-md-6 col-lg-6">
+                        <div class="col-6 col-sm-6 col-md-6 col-lg-6">
                             <img class="img-fluid productImage" src="assets/images/square300x300.png">
                         </div>
-                        <div class="col-6 col-sm-12 col-md-6 col-lg-6">
+                        <div class="col-6 col-sm-6 col-md-6 col-lg-6">
                             <img class="img-fluid productImage" src="assets/images/square300x300.png">
                         </div>
-                        <div class="col-6 col-sm-12 col-md-6 col-lg-6">
+                        <div class="col-6 col-sm-6 col-md-6 col-lg-6">
                             <img class="img-fluid productImage" src="assets/images/square300x300.png">
                         </div>
-                        <div class="col-6 col-sm-12 col-md-6 col-lg-6">
+                        <div class="col-6 col-sm-6 col-md-6 col-lg-6">
                             <img class="img-fluid productImage" src="assets/images/square300x300.png">
                         </div>
-                        <div class="col-6 col-sm-12 col-md-6 col-lg-6">
+                        <div class="col-6 col-sm-6 col-md-6 col-lg-6">
                             <img class="img-fluid productImage" src="assets/images/square300x300.png">
                         </div>
-                        <div class="col-6 col-sm-12 col-md-6 col-lg-6">
+                        <div class="col-6 col-sm-6 col-md-6 col-lg-6">
                             <img class="img-fluid productImage" src="assets/images/square300x300.png">
                         </div>
                     </div>
@@ -129,39 +130,91 @@
         </div>
         <div class="container">
             <div class="row">
+                <div class="col-sm-12 col-md-6 col-lg-6 hideScroll text-center" style="height: 950px;  overflow: auto;">
+                    <div class="row">
+                        <div class="col-6 col-sm-6 col-md-6 col-lg-6">
+                            <img class="img-fluid productImage" src="assets/images/square300x300.png">
+                        </div>
+                        <div class="col-6 col-sm-6 col-md-6 col-lg-6">
+                            <img class="img-fluid productImage" src="assets/images/square300x300.png">
+                        </div>
+                        <div class="col-6 col-sm-6 col-md-6 col-lg-6">
+                            <img class="img-fluid productImage" src="assets/images/square300x300.png">
+                        </div>
+                        <div class="col-6 col-sm-6 col-md-6 col-lg-6">
+                            <img class="img-fluid productImage" src="assets/images/square300x300.png">
+                        </div>
+                        <div class="col-6 col-sm-6 col-md-6 col-lg-6">
+                            <img class="img-fluid productImage" src="assets/images/square300x300.png">
+                        </div>
+                        <div class="col-6 col-sm-6 col-md-6 col-lg-6">
+                            <img class="img-fluid productImage" src="assets/images/square300x300.png">
+                        </div>
+                        <div class="col-6 col-sm-6 col-md-6 col-lg-6">
+                            <img class="img-fluid productImage" src="assets/images/square300x300.png">
+                        </div>
+                        <div class="col-6 col-sm-6 col-md-6 col-lg-6">
+                            <img class="img-fluid productImage" src="assets/images/square300x300.png">
+                        </div>
+                        <div class="col-6 col-sm-6 col-md-6 col-lg-6">
+                            <img class="img-fluid productImage" src="assets/images/square300x300.png">
+                        </div>
+                        <div class="col-6 col-sm-6 col-md-6 col-lg-6">
+                            <img class="img-fluid productImage" src="assets/images/square300x300.png">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-12 col-md-6 col-lg-6">
+                    <img class="img-fluid landingProductHeroImage" src="assets/images/650x950.png">
+                </div>
+            </div>
+        </div>
+
+    </section>
+
+
+    <section>
+
+        <div class="container-fluid text-center">
+            <div class="row campaignTitle">
+                <div class="my-auto">Campaign Title</div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
                 <div class="col-sm-12 col-md-6 col-lg-6">
                     <img class="img-fluid landingProductHeroImage" src="assets/images/650x950.png">
                 </div>
                 <div class="col-sm-12 col-md-6 col-lg-6 hideScroll text-center" style="height: 950px;  overflow: auto;">
                     <div class="row">
-                        <div class="col-6 col-sm-12 col-md-6 col-lg-6">
+                        <div class="col-6 col-sm-6 col-md-6 col-lg-6">
                             <img class="img-fluid productImage" src="assets/images/square300x300.png">
                         </div>
-                        <div class="col-6 col-sm-12 col-md-6 col-lg-6">
+                        <div class="col-6 col-sm-6 col-md-6 col-lg-6">
                             <img class="img-fluid productImage" src="assets/images/square300x300.png">
                         </div>
-                        <div class="col-6 col-sm-12 col-md-6 col-lg-6">
+                        <div class="col-6 col-sm-6 col-md-6 col-lg-6">
                             <img class="img-fluid productImage" src="assets/images/square300x300.png">
                         </div>
-                        <div class="col-6 col-sm-12 col-md-6 col-lg-6">
+                        <div class="col-6 col-sm-6 col-md-6 col-lg-6">
                             <img class="img-fluid productImage" src="assets/images/square300x300.png">
                         </div>
-                        <div class="col-6 col-sm-12 col-md-6 col-lg-6">
+                        <div class="col-6 col-sm-6 col-md-6 col-lg-6">
                             <img class="img-fluid productImage" src="assets/images/square300x300.png">
                         </div>
-                        <div class="col-6 col-sm-12 col-md-6 col-lg-6">
+                        <div class="col-6 col-sm-6 col-md-6 col-lg-6">
                             <img class="img-fluid productImage" src="assets/images/square300x300.png">
                         </div>
-                        <div class="col-6 col-sm-12 col-md-6 col-lg-6">
+                        <div class="col-6 col-sm-6 col-md-6 col-lg-6">
                             <img class="img-fluid productImage" src="assets/images/square300x300.png">
                         </div>
-                        <div class="col-6 col-sm-12 col-md-6 col-lg-6">
+                        <div class="col-6 col-sm-6 col-md-6 col-lg-6">
                             <img class="img-fluid productImage" src="assets/images/square300x300.png">
                         </div>
-                        <div class="col-6 col-sm-12 col-md-6 col-lg-6">
+                        <div class="col-6 col-sm-6 col-md-6 col-lg-6">
                             <img class="img-fluid productImage" src="assets/images/square300x300.png">
                         </div>
-                        <div class="col-6 col-sm-12 col-md-6 col-lg-6">
+                        <div class="col-6 col-sm-6 col-md-6 col-lg-6">
                             <img class="img-fluid productImage" src="assets/images/square300x300.png">
                         </div>
                     </div>
@@ -172,14 +225,143 @@
     </section>
 
 
+    <section style="margin: 100px;">
+        <div class="container">
+            <div class="row justify-content-md-center">
+                <div class="col-12 col-sm-12 col-md-6 text-center accessoriesTitle">
+                    <h3>ACCESSORIES</h3>
+                </div>
+            </div>
+        </div>
+    </section>
 
 
+    <section style="margin: 100px;">
+        <div class="container">
+            <div class="row justify-content-md-around">
+                <div class="col-12 col-sm-12 col-md-5 text-center accessoriesTitle">
+                    <h5>Video Call Appointment</h5>
+                    <form action="" class="row g-3 needs-validation" novalidate>
+                        <div class="col-md-12">
+                            <label for="validationCustom01" class="form-label"></label>
+                            <input type="text" class="form-control" id="validationCustom01" placeholder="Name" required>
+                            <div class="valid-feedback">
+                                Looks good!
+                            </div>
+                            <div class="invalid-feedback">
+                                Please enter a Name.
+                            </div>
+                        </div>
+                        <div class="col-md-12">
+                            <label for="validationCustom02" class="form-label"></label>
+                            <input type="number" class="form-control" id="validationCustom02" placeholder="Contact No." required>
+                            <div class="valid-feedback">
+                                Looks good!
+                            </div>
+                            <div class="invalid-feedback">
+                                Please enter a Contact Number.
+                            </div>
+                        </div>
+                        <div class="col-md-12">
+                            <label for="validationCustom01" class="form-label"></label>
+                            <input type="email" class="form-control" id="validationCustom01" aria-describedby="emailHelpId" placeholder="Email" required>
+                            <div class="valid-feedback">
+                                Looks good!
+                            </div>
+                            <div class="invalid-feedback">
+                                Please enter Valid a Email.
+                            </div>
+                        </div>
+                        <div class="col-md-12">
+                            <label for="validationCustom02" class="form-label"></label>
+                            <input type="text" class="form-control" id="validationCustom02" placeholder="Message" required>
+                            <div class="valid-feedback">
+                                Looks good!
+                            </div>
+                            <div class="invalid-feedback">
+                                Please enter a Message.
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <button class="btn btn-primary" type="submit">Submit form</button>
+                        </div>
+                    </form>
+                </div>
+                <div class="col-12 col-sm-12 col-md-5 text-center accessoriesTitle">
+                    <h5>Follow Us</h5>
+                    <div class="row d-flex justify-content-center mt-5">
+                        <div class="col-md-2 text-center">
+                            <i class='bx bxl-facebook-square bx-md'></i>
+                        </div>
+                        <div class="col-md-2">
+                            <i class='bx bxl-instagram bx-md'></i>
+                        </div>
+                        <div class="col-md-2">
+                            <i class='bx bxl-whatsapp bx-md'></i>
+                        </div>
+                    </div>
+                    <div class="row d-flex justify-content-center mt-5">
+                        <h5>Contact Us</h5>
+                    </div>
+                    <div class="row d-flex justify-content-center mt-3">
+                        <i class='bx bxs-phone bx-md'></i>
+                        <h6 class="mt-3">+91 9427542226 / +91 265 2422622</h6>
+                    </div>
+                    <div class="row d-flex justify-content-center mt-3">
+                        <i class='bx bx-envelope bx-md'></i>
+                        <h6 class="mt-3">f2fusionfashion@gmail.com</h6>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 
-
+    <footer class="footer mt-auto py-3 bg-light border-top border-secondary border-1">
+        <div class="container">
+            <div class="row">
+                <!-- <span class="text-muted">Place sticky footer content here.</span> -->
+                <div class="col-md-3">
+                    <h6>@f2fusionfashion</h6>
+                </div>
+                <div class="col-md-3">
+                    <h6>2 Gamthi Complex, Productivity Road, Alkapuri, Vadodara - 390007</h6>
+                </div>
+                <div class="col-md-3">
+                    <h6>Navrang Cinema Road, Siyapura, Raopura, Mandvi, Vadodara - 390001</h6>
+                </div>
+                <div class="col-md-3">
+                    <h6>Privacy Policy | Terms & Conditions</h6>
+                </div>
+            </div>
+        </div>
+    </footer>
 
 
 
     <!-- Optional JavaScript; choose one of the two! -->
+
+    <script>
+        // Example starter JavaScript for disabling form submissions if there are invalid fields
+        (function() {
+            'use strict'
+
+            // Fetch all the forms we want to apply custom Bootstrap validation styles to
+            var forms = document.querySelectorAll('.needs-validation')
+
+            // Loop over them and prevent submission
+            Array.prototype.slice.call(forms)
+                .forEach(function(form) {
+                    form.addEventListener('submit', function(event) {
+                        if (!form.checkValidity()) {
+                            event.preventDefault()
+                            event.stopPropagation()
+                        }
+
+                        form.classList.add('was-validated')
+                    }, false)
+                })
+        })()
+    </script>
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
